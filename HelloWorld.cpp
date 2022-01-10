5d4fa3f8da2f9d25c1a1ab04f042a105d079bdf9
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2022-01-10 20:58:22 +0530

    Hello World

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+// c++ program to display Hello World message
+// #iostream is called a directive, it tells the compiler to include iostream file which contains declarations of all standard input/ output library functions.
+
+#include <iostream>
+using namespace std;
+
+// Main() function: where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout << "Hello World";
+
+    return 0;
+}
